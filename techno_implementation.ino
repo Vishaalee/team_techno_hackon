a3a72a039f7422bad55d3458b7dd84ec8fc8082c
Vishaalee <30550677+Vishaalee@users.noreply.github.com>
2017-08-05 07:09:37 +0530

    Add files via upload

---
+++ techno_implementation.ino
@@ -0,0 +1,26 @@
+float temp0;
+int tempPin0 = A0;
+void setup()
+{
+
+Serial.begin(9600);
+pinMode(5, OUTPUT);
+pinMode(4, OUTPUT);
+
+}
+
+void loop()
+
+{
+digitalWrite(4, 1);
+temp0 = analogRead(tempPin0);
+temp0 = temp0 * 0.48828125;
+Serial.print(temp0);
+Serial.println("*C");
+if(temp0>32)
+digitalWrite(5, 1); 
+else
+digitalWrite(5, 0); 
+delay(1000);
+
+}
