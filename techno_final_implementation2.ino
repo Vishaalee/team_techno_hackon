be7f5d94f9f4700ed30e5e05a3efcb87ce7af1a0
Vishaalee <30550677+Vishaalee@users.noreply.github.com>
2017-08-05 08:52:13 +0530

    Add files via upload

---
+++ techno_final_implementation2.ino
@@ -0,0 +1,63 @@
+#include <Time.h>
+#include <TimeLib.h>
+int r,s,count=0;
+time_t t;
+float temp0;
+int tempPin0 = A0;
+
+void setup()
+{
+
+Serial.begin(9600);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+}
+
+void loop()
+
+{
+  digitalWrite(6,1);
+  temp0=analogRead(tempPin0);
+  temp0=temp0*0.48828125;
+time_t t=now();
+s=second(t);
+r=minute(t);
+Serial.println(r);
+Serial.println(s);
+Serial.println(temp0);
+
+if(temp0>28 && temp0<40)
+{
+count=0;
+digitalWrite(5,1);
+digitalWrite(4,0);
+Serial.println("Phase2 is on");
+if(r>=1 && r<2)
+  {
+  digitalWrite(4,1);
+  Serial.println("Burglar Alarm on");
+}
+else
+{
+  digitalWrite(4,0);
+}}
+else if(temp0>32)
+{
+  digitalWrite(5,0);
+digitalWrite(4,1);
+Serial.println("Fire alarm");
+}
+else
+{
+count++;
+if(count>2)
+{
+  digitalWrite(5,0);
+  digitalWrite(4,0);
+  Serial.println("time limit");
+}
+}
+delay(1000);
+}
+
