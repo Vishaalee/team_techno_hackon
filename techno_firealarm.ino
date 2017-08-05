3f5eb9e76a0e2ae28ad4f7a9e32ac0ec36c8f4c9
Vishaalee <30550677+Vishaalee@users.noreply.github.com>
2017-08-05 08:59:38 +0530

    Add files via upload

---
+++ techno_firealarm.ino
@@ -0,0 +1,38 @@
+float temp0;
+int tempPin0 = A0;
+int i,count=0,count1=0;
+void setup()
+{
+
+Serial.begin(9600);
+pinMode(5, OUTPUT);
+pinMode(4, OUTPUT);
+
+}
+
+void loop()
+
+{
+temp0 = analogRead(tempPin0);
+temp0 = temp0 * 0.48828125;
+Serial.print(temp0);
+Serial.println("*C");
+
+if(temp0>28 && temp0<50)
+{
+digitalWrite(5,1);
+digitalWrite(4,0);
+}
+else if(temp0>=50)
+{
+digitalWrite(4,1);
+digitalWrite(5,0);
+}
+else
+{
+  digitalWrite(5,0);
+digitalWrite(4,0);
+}
+delay(1000);
+}
+
